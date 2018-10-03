0b8d880a6579e2dfe83eb11d01c56c43cb94efe0
Axel Alavedra Cabello <axel.alavedra@gmail.com>
2018-10-03 13:56:57 +0200

    String = operator fix

--- String.h
+++ String.h
@@ -5,58 +5,59 @@
 
 class String {
 private:
-	unsigned int allocated_memory;
-	char* string;
+	unsigned int allocated_memory = 0u;
+	char* string = nullptr;
 
 	void Allocate(unsigned int size) {
 		allocated_memory = size;
 		string = new char[allocated_memory];
 	}
 public:
-	String() {
-		Allocate(1);
-	}
+	String() {}
 	String(const char* str) {
 		assert(str != nullptr);
 		if (str != nullptr) {
 			Allocate(strlen(str)+1);
 			strcpy(string,str);
 		}
-		else {
-			Allocate(1);
-		}
 	}
 	String(const String& str) {
-		assert(str.GetString() != nullptr);
-		if (str.GetString() != nullptr) {
+		assert(str.string != nullptr);
+		if (str.string != nullptr) {
 			Allocate(str.allocated_memory + 1);
-			strcpy(string, str.GetString());
-		}
-		else {
-			Allocate(1);
+			strcpy(string, str.string);
 		}
 
+	}
+	~String() {
+		if (string != nullptr)
+		{
+			delete[] string;
+			string = nullptr;
+		}
 	}
 
 	char* GetString() const {
 		return string;
 	}	
 
-	String& operator= (const char* str)
+	char* operator= (const char* str)
 	{
 		assert(str != nullptr);
 		if (str != nullptr) {
+			if (string != nullptr) delete[] string;
 			Allocate(strlen(str) + 1);
 			strcpy(string, str);
 		}
-		return *this;
+		return string;
 	}
-	String& operator= (const String& str)
+	String operator= (const String& str)
 	{
-		assert(str.GetString() != nullptr);
-		if (str.GetString() != nullptr) {
+		assert(str.string != nullptr);
+		if (str.string != nullptr) {
+			if (string != nullptr) delete[] string;
 			Allocate(str.allocated_memory + 1);
-			strcpy(string, str.GetString());
+			strcpy(string, str.string);
 		}
 		return *this;
 	}
